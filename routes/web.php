c9765ee64e750e3ce045a03c310a8ea01a17459b
Alex <alexzarodaeli8@gmail.com>
2026-05-15 22:36:02 +0700

    pergantian

--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,9 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
 Route::view('pos', 'pos')
     ->middleware(['auth'])
     ->name('pos');
-
-Route::view('products', 'products')
-    ->middleware(['auth'])
-    ->name('products');
-
-Route::view('categories', 'categories')
-    ->middleware(['auth'])
-    ->name('categories');
 
 Route::view('orders', 'orders')
     ->middleware(['auth'])
@@ -32,12 +20,10 @@ Route::view('kitchen', 'kitchen')
     ->middleware(['auth'])
     ->name('kitchen');
 
-Route::view('settings', 'settings')
-    ->middleware(['auth'])
-    ->name('settings');
-
-Route::view('users', 'users')
-    ->middleware(['auth'])
-    ->name('users');
+Route::view('profile', 'profile')->middleware(['auth'])->name('profile');
+Route::view('products', 'products')->middleware(['auth'])->name('products');
+Route::view('categories', 'categories')->middleware(['auth'])->name('categories');
+Route::view('settings', 'settings')->middleware(['auth'])->name('settings');
+Route::view('users', 'users')->middleware(['auth', \App\Http\Middleware\IsAdmin::class])->name('users');
 
 require __DIR__.'/auth.php';
